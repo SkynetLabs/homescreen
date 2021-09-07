ccc6c218b59909b65339eff08fd318f660dedcdf
Karol Wypchlo <kwypchlo@gmail.com>
2021-09-07 13:13:07 +0200

    footer

--- src/pages/Homescreen.js
+++ src/pages/Homescreen.js
@@ -51,7 +51,8 @@ export default function Homescreen() {
           </div>
         </div>
       </Disclosure>
-      <div className="py-10">
+
+      <div className="py-10 flex-1">
         <header>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
             <h1 className="text-3xl font-bold leading-tight text-gray-900">Your Homescreen</h1>
