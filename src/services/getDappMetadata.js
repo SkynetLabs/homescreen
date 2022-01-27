b2a7bfedb13953536b62f5b9e223d962eb820a73
Daniel Helm <dan.helm@gmail.com>
2022-01-27 09:53:21 -0600

    replace ky-universal with ky, restore ky code

--- src/services/getDappMetadata.js
+++ src/services/getDappMetadata.js
@@ -1,3 +1,4 @@
+import ky from "ky";
 import { getMetadata } from "page-metadata-parser";
 import ogs from "open-graph-scraper-lite";
 import skynetClient from "../services/skynetClient";
@@ -37,9 +38,10 @@ export default async function getDappMetadata(skylink) {
   try {
     // grab skylinkUrl for use in fetching metadata.
     const skylinkUrl = await skynetClient.getSkylinkUrl(skylink, { subdomain: true });
-    const contentType = (await fetch(skylinkUrl, { method: "HEAD", credentials: "include" })).headers.get(
-      "content-type"
-    );
+    const contentType = (await ky.head(skylinkUrl, { credentials: "include" })).headers.get("content-type");
+    // const contentType = (await fetch(skylinkUrl, { method: "HEAD", credentials: "include" })).headers.get(
+    //   "content-type"
+    // );
 
     if (contentType !== "text/html") {
       return { ...emptyManifest, ...skynetMetadata };
@@ -49,7 +51,8 @@ export default async function getDappMetadata(skylink) {
     // TODO: replace with client.getFileContent() for registry verification on resolver skylinks
 
     // Grab HTML and parse. Used to find manifest URL and metadata.
-    const responseText = await (await fetch(skylinkUrl, { method: "GET", credentials: "include" })).text();
+    const responseText = await ky.get(skylinkUrl, { credentials: "include" }).text();
+    // const responseText = await (await fetch(skylinkUrl, { method: "GET", credentials: "include" })).text();
     const parser = new DOMParser();
     const doc = parser.parseFromString(responseText, "text/html");
 
